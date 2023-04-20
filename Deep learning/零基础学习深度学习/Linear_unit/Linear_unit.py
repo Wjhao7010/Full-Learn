262d43ef9f1f29305156b0127bd9be920bb50f8b
Consigliere <76636190+Wjhao7010@users.noreply.github.com>
2023-04-20 23:18:27 +0800

    Update Linear_unit.py

--- Deep learning/零基础学习深度学习/Linear_unit/Linear_unit.py
+++ Deep learning/零基础学习深度学习/Linear_unit/Linear_unit.py
@@ -1,5 +1,6 @@
 from perceptron import Perceptron
-
+# 监督学习类型  运用员工已知工龄和工资来训练线性单元
+# 继承了Perceptron
 # 定义激活函数f
 f = lambda x: x
 
@@ -14,9 +15,9 @@ def get_training_dataset():
     创建5个人的收入数据
     """
     # 构建训练数据
-    # 输入向量列表，每一项是工作年限
+    # 输入向量列表，每项是工作年限
     input_vecs = [[5], [3], [8], [1.4], [10.1]]
-    # 期望的输出列表，月薪，注意要与输入一一对应
+    # 期望的输出列表，月薪要与输入一一对应
     labels = [5500, 2300, 7600, 1800, 11400]
     return input_vecs, labels
 
@@ -25,7 +26,7 @@ def train_linear_unit():
     # 使用数据训练线性单元
     # 创建感知器，输入参数的特征数为1（工作年限）
     lu = LinearUnit(1)
-    # 训练，迭代10轮, 学习速率为0.01
+    # 训练并且迭代10轮, 学习速率为0.01
     input_vecs, labels = get_training_dataset()
     lu.train(input_vecs, labels, 10, 0.01)
     # 返回训练好的线性单元
@@ -33,7 +34,9 @@ def train_linear_unit():
 
 
 if __name__ == '__main__':
-    '''训练线性单元'''
+    """
+    训练线性单元
+    """
     linear_unit = train_linear_unit()
     # 打印训练获得的权重
     print(linear_unit)
